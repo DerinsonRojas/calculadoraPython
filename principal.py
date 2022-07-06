bb568f4b46361dd19a0a9efa2f5474ebd7ad3132
Derinson Rojas <derinsonrojas@gmail.com>
2022-07-06 23:50:05 +0200

    Mejora de imagen, botones más grandes

--- principal.py
+++ principal.py
@@ -1,9 +1,10 @@
 from tkinter import *
+from turtle import title
 
 
 raiz=Tk()
-raiz.config(padx=30, pady=30, background="green" )
-
+raiz.config(padx=30, pady=30, background="green")
+raiz.title('Calculadora')
 miFrame=Frame(raiz)
 
 miFrame.pack()
@@ -206,56 +207,55 @@ def borrarUltimo(num):
 
 #--------------Pantalla-----------------------
 pantalla=Entry(miFrame, textvariable=numeroPantalla)
-pantalla.grid(row=1,column=1,padx=10,pady=10, columnspan=4)
-pantalla.config(fg="#4D1906", justify="right")
+pantalla.grid(row=1,column=1,padx=5,pady=5, columnspan=4)
+pantalla.config(fg="#4D1906", justify="right", width=25, font=40)
 
 #--------------Fila 1---------------------------
-boton7=Button(miFrame,text='7',width=3, command=lambda:botonPresionado('7'))
+boton7=Button(miFrame,text='7',width=7, height=3, command=lambda:botonPresionado('7'))
 boton7.grid(row=2,column=1)
-boton8=Button(miFrame,text='8',width=3, command=lambda:botonPresionado('8'))
+boton8=Button(miFrame,text='8',width=7, height=3, command=lambda:botonPresionado('8'))
 boton8.grid(row=2,column=2)
-boton9=Button(miFrame,text='9',width=3, command=lambda:botonPresionado('9'))
+boton9=Button(miFrame,text='9',width=7, height=3, command=lambda:botonPresionado('9'))
 boton9.grid(row=2,column=3)
-botonDiv=Button(miFrame,text='/',width=3, command=lambda:division(numeroPantalla.get()))
+botonDiv=Button(miFrame,text='/',width=7, height=3, command=lambda:division(numeroPantalla.get()))
 botonDiv.grid(row=2,column=4)
 
 #--------------Fila 2---------------------------
-boton4=Button(miFrame,text='4',width=3, command=lambda:botonPresionado('4'))
+boton4=Button(miFrame,text='4',width=7, height=3, command=lambda:botonPresionado('4'))
 boton4.grid(row=3,column=1)
-boton5=Button(miFrame,text='5',width=3, command=lambda:botonPresionado('5'))
+boton5=Button(miFrame,text='5',width=7, height=3, command=lambda:botonPresionado('5'))
 boton5.grid(row=3,column=2)
-boton6=Button(miFrame,text='6',width=3, command=lambda:botonPresionado('6'))
+boton6=Button(miFrame,text='6',width=7, height=3, command=lambda:botonPresionado('6'))
 boton6.grid(row=3,column=3)
-botonMult=Button(miFrame,text='x',width=3, command=lambda:multiplicacion(numeroPantalla.get()))
+botonMult=Button(miFrame,text='x',width=7, height=3, command=lambda:multiplicacion(numeroPantalla.get()))
 botonMult.grid(row=3,column=4)
 
 #--------------Fila 3---------------------------
-boton1=Button(miFrame,text='1',width=3, command=lambda:botonPresionado('1'))
+boton1=Button(miFrame,text='1',width=7, height=3, command=lambda:botonPresionado('1'))
 boton1.grid(row=4,column=1)
-boton2=Button(miFrame,text='2',width=3, command=lambda:botonPresionado('2'))
+boton2=Button(miFrame,text='2',width=7, height=3, command=lambda:botonPresionado('2'))
 boton2.grid(row=4,column=2)
-boton3=Button(miFrame,text='3',width=3, command=lambda:botonPresionado('3'))
+boton3=Button(miFrame,text='3',width=7, height=3, command=lambda:botonPresionado('3'))
 boton3.grid(row=4,column=3)
-botonRest=Button(miFrame,text='-',width=3, command=lambda:resta(numeroPantalla.get()))
+botonRest=Button(miFrame,text='-',width=7, height=3, command=lambda:resta(numeroPantalla.get()))
 botonRest.grid(row=4,column=4)
 
 #--------------Fila 4---------------------------
-botonComa=Button(miFrame,text='.',width=3, command=lambda:botonPresionado('.'))
+botonComa=Button(miFrame,text='.',width=7, height=3, command=lambda:botonPresionado('.'))
 botonComa.grid(row=5,column=1)
-boton0=Button(miFrame,text='0',width=3, command=lambda:botonPresionado('0'))
+boton0=Button(miFrame,text='0',width=7, height=3, command=lambda:botonPresionado('0'))
 boton0.grid(row=5,column=2)
-botonCambiaSigno=Button(miFrame,text='+/-',width=3, command=lambda:botonPresionado(cambiaSigno(numeroPantalla.get())))
+botonCambiaSigno=Button(miFrame,text='+/-',width=7, height=3, command=lambda:botonPresionado(cambiaSigno(numeroPantalla.get())))
 botonCambiaSigno.grid(row=5,column=3)
-botonSuma=Button(miFrame,text='+',width=3, command=lambda:suma(numeroPantalla.get()))
+botonSuma=Button(miFrame,text='+',width=7, height=3, command=lambda:suma(numeroPantalla.get()))
 botonSuma.grid(row=5,column=4)
 
 #--------------Fila 5--------------------------
-botonBorrarUltimo=Button(miFrame,text='<-',width=3, command=lambda:botonPresionado(borrarUltimo(numeroPantalla.get())))
+botonBorrarUltimo=Button(miFrame,text='<-',width=7, height=3, command=lambda:botonPresionado(borrarUltimo(numeroPantalla.get())))
 botonBorrarUltimo.grid(row=6,column=1)
-botonBorrar=Button(miFrame,text='CE',width=3, command=borrarPantalla)
+botonBorrar=Button(miFrame,text='CE',width=7, height=3, command=borrarPantalla)
 botonBorrar.grid(row=6,column=2)
-botonIgual=Button(miFrame,text='=',width=3, command=lambda:botonPresionado(operacionResultado()))
-botonIgual.grid(row=6,column=4, ipadx=10)
-
+botonIgual=Button(miFrame,text='=',width=15,height=3, command=lambda:botonPresionado(operacionResultado()))
+botonIgual.grid(row=6,column=3, columnspan=2)
 
 raiz.mainloop()
